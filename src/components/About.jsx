947f4b24b1308bdc54a0519bab0770b29da41e99
jayshiai <jayvardhanpatil7811@gmail.com>
2023-09-19 01:41:22 +0530

    Tag component added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,10 @@
+import { HiArrowRight } from "react-icons/hi";
+import Tag from "./bytes/Tag";
 const About = () => {
   return (
-    <div className="lg:w-[75vw] translate-y-[-20vh]">
-      <div className="ml-[7%] w-[200px] rounded-full border-purple-700 border-2 h-[60px] "></div>
+    <div className="ml-[10%]  lg:w-[75vw] lg:translate-y-[-20vh]">
+      <Tag text="About" icon={<HiArrowRight />} />
+      <div></div>
     </div>
   );
 };
