5ede74674edf866024da4b5552566b208e9d840f
jayshiai <jayvardhanpatil7811@gmail.com>
2023-09-18 23:50:33 +0530

    trailed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import MouseTrail from "@/components/MouseTrail";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
